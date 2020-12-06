b26e07c650c8993b1e1ba18b03280cdac541fe50
kosba <mohamed.kosba96@gmail.com>
2020-12-06 17:33:46 +0200

    Use StreamController To Consume Data (Out & In)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,15 +21,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Stream<int> getNumbers() async* {
-  for(int i = 1; i <= 4; i ++){
-    yield i;
-    await Future.delayed(Duration(seconds: 3));
+class HomeModel {
+  final StreamController<int> _numbersController = StreamController<int>.broadcast();
+  Stream<int> get outNumbers => _numbersController.stream;
+  Sink<int> get inNumbers => _numbersController.sink;
+
+  void dispose (){
+    _numbersController.close();
   }
 }
 
 
+
 class MyHomePage extends StatelessWidget {
+
+  final model = HomeModel();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,19 +48,57 @@ class MyHomePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: StreamBuilder<int>(
-          stream: getNumbers()
-            .where((event) => event == 2 ),
-          builder: (context, snapshot) {
-            return Text(
-              snapshot.hasData ? snapshot.data.toString() :"no Data here",
-              style: TextStyle(
-                fontSize: 50.0
-              ),
-            );
-          }
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          ReadStreamWidget(model),
+          SizedBox(
+            height: 20.0,
+          ),
+          AddStreamWidget(model)
+          ],
         ),
       ),
+    );
+  }
+}
+
+class AddStreamWidget extends StatelessWidget {
+  const AddStreamWidget(this._model);
+
+  final HomeModel _model;
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: Text('Click Me',
+        style: TextStyle(
+            fontSize: 48.0
+        ),),
+      onPressed: () {
+        _model.inNumbers.add(Random().nextInt(2000));
+      }
+      ,);
+  }
+}
+
+class ReadStreamWidget extends StatelessWidget {
+  const ReadStreamWidget(this._model);
+
+  final HomeModel _model;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<int>(
+      stream: _model.outNumbers,
+      builder: (context, snapshot) {
+        return Text(
+          snapshot.hasData ? snapshot.data.toString() :"no Data here",
+        style: TextStyle(
+            fontSize: 45.0
+          ),
+        );
+      }
     );
   }
 }
